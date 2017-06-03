7bf18b1eb1d8b51a434e078494e53a3080c6aa2d
jsudufour <jsudufour@gmail.com>
2017-06-03 14:39:21 -0400

    Add post request endpoint and relevant test

--- posts/api.py
+++ posts/api.py
@@ -68,3 +68,21 @@ def post_delete(id):
     session.delete(post)
     session.commit()
     return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/posts", methods=["POST"])
+@decorators.accept("application/json")
+def posts_post():
+    """ Add a new post """
+    data = request.json
+
+    # Add the post to the database
+    post = models.Post(title=data["title"], body=data["body"])
+    session.add(post)
+    session.commit()
+
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 201, headers=headers,
+                    mimetype="application/json")
